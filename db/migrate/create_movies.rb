2b6f4b3fc48e1bac7859d44a2d5f08c59b537e08
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-21 20:38:43 +0000

    Automatically backed up by Learn

--- db/migrate/create_movies.rb
+++ db/migrate/create_movies.rb
@@ -1,1 +1,11 @@
-rake db:create_migration NAME=create_movies
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.text :title
+      t.integer :release_date
+      t.text :director
+      t.text :lead
+      t.boolean :in_theaters
+    end 
+  end
+end
